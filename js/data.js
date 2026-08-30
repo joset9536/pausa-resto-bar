24d06e541c7f5021afedc8391b86a201984b0bb5
joset9536 <joset9536@gmail.com>
2026-08-30 16:35:59 -0300

    data source

---
+++ js/data.js
@@ -0,0 +1,31 @@
+window.PAUSA = {
+  name: "PAUSA",
+  city: "San Ramón de la Nueva Orán",
+  province: "Salta",
+  street: "25 de Mayo",
+  corner: "Alvarado y 25 de Mayo",
+  plusCode: "VM8G+F6",
+  phoneDisplay: "3878 53-4824",
+  phoneTel: "+543878534824",
+  whatsapp: "543878534824",
+  maps: "https://maps.app.goo.gl/q5cQG7UDGaiB5eXz5",
+  instagram: "https://www.instagram.com/pausa.restobar.oran/",
+  facebook: "https://www.facebook.com/pausacaferestob",
+  rating: "4.2",
+  reviews: "629",
+  hero: [
+    { src: "img/hero-noche.jpg", alt: "PAUSA de noche, Alvarado y 25 de Mayo", label: "Noche" },
+    { src: "img/hero-dia.jpg", alt: "PAUSA de día, Alvarado y 25 de Mayo", label: "Día" }
+  ],
+  favoritos: [
+    { src: "img/food-sandwich.jpg", title: "Sándwich de la casa" },
+    { src: "img/food-tostado.jpg", title: "Tostado y jugo" },
+    { src: "img/food-brindis.jpg", title: "Barra" },
+    { src: "img/food-ensalada.jpg", title: "Ensalada" }
+  ],
+  patio: [
+    { src: "img/hero-noche.jpg", title: "La esquina de noche" },
+    { src: "img/food-brindis.jpg", title: "La mesa" },
+    { src: "img/hero-dia.jpg", title: "De día" }
+  ]
+};
